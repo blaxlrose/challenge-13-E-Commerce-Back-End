99e6a88f2070fe17e46de41eab9a1e3438a2f02d
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-07-12 15:18:03 -0400

    indexjs

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+ })
+ 
+ // Categories have many Products
+ Category.hasMany(Product, {
+   foreignKey: 'category_id'
+ })
+ 
+ // Products belongToMany Tags (through ProductTag)
+ Product.belongsToMany(Tag, {
+   through: {
+     foreignKey: 'product_id',
+     model: ProductTag,
+     unique: false
+   },
+   as: 'product_detail'
+ })
+ 
+ // Tags belongToMany Products (through ProductTag)
+ Tag.belongsToMany(Product, {
+   through: {
+     foreignKey: 'tag_id',
+     model: ProductTag,
+     unique: false
+   },
+   as: 'tag_detail'
+ })
+ 
 
 module.exports = {
   Product,
