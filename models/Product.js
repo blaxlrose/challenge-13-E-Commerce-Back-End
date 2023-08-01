f824162ae384a109e82da5b40a1c0595a8d16f79
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-08-01 00:51:40 -0400

    product.js

--- models/Product.js
+++ models/Product.js
@@ -7,9 +7,42 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// define columns
+
 Product.init(
   {
-    // define columns
+     id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -19,5 +52,7 @@ Product.init(
     modelName: 'product',
   }
 );
+
+  
 
 module.exports = Product;
