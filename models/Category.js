f04d92b45be559c68fb312e30da39b5dd2dffe50
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-08-01 00:37:07 -0400

    category.js finished

--- models/Category.js
+++ models/Category.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Define columns
 Category.init(
   {
     id: {
@@ -12,8 +13,13 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
 
-  
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
